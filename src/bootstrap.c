680ed80011bd0b93bd8b6ae1371110081bce30d2
dotxnc <lumaioteon@gmail.com>
2022-06-07 07:42:35 -0400

    Key and mouse input functions

--- src/bootstrap.c
+++ src/bootstrap.c
@@ -1,7 +1,7 @@
 #include "bootstrap.h"
 #include "e4core.h"
 #include <GLFW/glfw3.h>
-#include <stdbool.h>
+#include <string.h>
 
 static GLFWwindow* _window = NULL;
 static BootstrapWindowSettingsT _window_settings;
@@ -9,9 +9,34 @@ static BootstrapEngineSettingsT _engine_settings;
 static bool _running = true;
 static f32 _timer = 0.;
 
+typedef enum _KeyCheckE
+{
+    _KeyCheck_None,
+    _KeyCheck_Pressed,
+    _KeyCheck_Released,
+} _KeyCheckE;
+
+static InputActionE _keys[InputKey_Count];
+static _KeyCheckE _keycheck[InputKey_Count];
+
+static InputActionE _mouse[3];
+static _KeyCheckE _mousecheck[3];
+
 static void _bootstrap_key(GLFWwindow* window, i32 key, i32 scan, i32 action, i32 mods)
 {
     _window_settings.callbacks.key(key, action, mods);
+    _keys[key] = action;
+    switch (action)
+    {
+    case InputAction_Press:
+        _keycheck[key] = _KeyCheck_Pressed;
+        break;
+    case InputAction_Release:
+        _keycheck[key] = _KeyCheck_Released;
+        break;
+    default:
+        break;
+    };
 }
 
 static void _bootstrap_click(GLFWwindow* window, i32 button, i32 action, i32 mods)
@@ -19,13 +44,25 @@ static void _bootstrap_click(GLFWwindow* window, i32 button, i32 action, i32 mod
     f64 x, y;
     glfwGetCursorPos(_window, &x, &y);
     _window_settings.callbacks.click(x, y, action, button);
+
+    switch (action)
+    {
+    case InputAction_Press:
+        _mousecheck[button] = _KeyCheck_Pressed;
+        break;
+    case InputAction_Release:
+        _mousecheck[button] = _KeyCheck_Released;
+        break;
+    default:
+        break;
+    };
 }
 
 static void _bootstrap_char() {}
 
 static void _bootstrap_cursor(GLFWwindow* window, f64 x, f64 y)
 {
-    e4core_cursor(x, y);
+    e4core_set_cursor(x, y);
 }
 
 void bootstrap_init(BootstrapWindowSettingsT window_settings, BootstrapEngineSettingsT engine_settings)
@@ -99,4 +136,54 @@ void bootstrap_stop()
 f32 bootstrap_time()
 {
     return _timer;
+}
+
+bool bootstrap_key_pressed(InputKeyE key)
+{
+    if (_keycheck[key] == _KeyCheck_Pressed)
+    {
+        _keycheck[key] = _KeyCheck_None;
+        return true;
+    }
+    return false;
+}
+
+bool bootstrap_key_released(InputKeyE key)
+{
+    if (_keycheck[key] == _KeyCheck_Released)
+    {
+        _keycheck[key] = _KeyCheck_None;
+        return true;
+    }
+    return false;
+}
+
+bool bootstrap_key_down(InputKeyE key)
+{
+    return glfwGetKey(_window, key) == GLFW_PRESS;
+}
+
+bool bootstrap_mouse_pressed(MouseButtonE button)
+{
+    if (_mousecheck[button] == _KeyCheck_Pressed)
+    {
+        _mousecheck[button] = _KeyCheck_None;
+        return true;
+    }
+    return false;
+}
+
+bool bootstrap_mouse_released(MouseButtonE button)
+{
+    if (_mousecheck[button] == _KeyCheck_Pressed)
+    {
+        _mousecheck[button] = _KeyCheck_None;
+        return true;
+    }
+    return false;
+}
+
+bool bootstrap_mouse_down(MouseButtonE button)
+{
+    return glfwGetMouseButton(_window, button);
 }
--- src/bootstrap.h
+++ src/bootstrap.h
@@ -17,6 +17,7 @@
  */
 
 #include "e4util.h"
+#include <stdbool.h>
 
 typedef enum InputActionE
 {
@@ -40,6 +41,130 @@ typedef enum MouseButtonE
     MouseButton_Right = 1,
     MouseButton_Middle = 2,
 } MouseButtonE;
+
+typedef enum InputKeyE
+{
+    InputKey_Unknown = -1,
+    InputKey_Space = 32,
+    InputKey_Apostrophe = 39,
+    InputKey_Comma = 44,
+    InputKey_Minus = 45,
+    InputKey_Period = 46,
+    InputKey_Slash = 47,
+    InputKey_0 = 48,
+    InputKey_1 = 49,
+    InputKey_2 = 50,
+    InputKey_3 = 51,
+    InputKey_4 = 52,
+    InputKey_5 = 53,
+    InputKey_6 = 54,
+    InputKey_7 = 55,
+    InputKey_8 = 56,
+    InputKey_9 = 57,
+    InputKey_Semicolon = 59,
+    InputKey_Equal = 61,
+    InputKey_A = 65,
+    InputKey_B = 66,
+    InputKey_C = 67,
+    InputKey_D = 68,
+    InputKey_E = 69,
+    InputKey_F = 70,
+    InputKey_G = 71,
+    InputKey_H = 72,
+    InputKey_I = 73,
+    InputKey_J = 74,
+    InputKey_K = 75,
+    InputKey_L = 76,
+    InputKey_M = 77,
+    InputKey_N = 78,
+    InputKey_O = 79,
+    InputKey_P = 80,
+    InputKey_Q = 81,
+    InputKey_R = 82,
+    InputKey_S = 83,
+    InputKey_T = 84,
+    InputKey_U = 85,
+    InputKey_V = 86,
+    InputKey_W = 87,
+    InputKey_X = 88,
+    InputKey_Y = 89,
+    InputKey_Z = 90,
+    InputKey_LeftBracket = 91,
+    InputKey_Backslash = 92,
+    InputKey_RightBracket = 93,
+    InputKey_Grave = 96,
+    InputKey_Escape = 256,
+    InputKey_Enter = 257,
+    InputKey_Tab = 258,
+    InputKey_Backspace = 259,
+    InputKey_Insert = 260,
+    InputKey_Delete = 261,
+    InputKey_Right = 262,
+    InputKey_Left = 263,
+    InputKey_Down = 264,
+    InputKey_Up = 265,
+    InputKey_PageUp = 266,
+    InputKey_PageDown = 267,
+    InputKey_Home = 268,
+    InputKey_End = 269,
+    InputKey_CapsLock = 280,
+    InputKey_ScrollLock = 281,
+    InputKey_NumLock = 282,
+    InputKey_PrintScreen = 283,
+    InputKey_Pause = 284,
+    InputKey_F1 = 290,
+    InputKey_F2 = 291,
+    InputKey_F3 = 292,
+    InputKey_F4 = 293,
+    InputKey_F5 = 294,
+    InputKey_F6 = 295,
+    InputKey_F7 = 296,
+    InputKey_F8 = 297,
+    InputKey_F9 = 298,
+    InputKey_F10 = 299,
+    InputKey_F11 = 300,
+    InputKey_F12 = 301,
+    InputKey_F13 = 302,
+    InputKey_F14 = 303,
+    InputKey_F15 = 304,
+    InputKey_F16 = 305,
+    InputKey_F17 = 306,
+    InputKey_F18 = 307,
+    InputKey_F19 = 308,
+    InputKey_F20 = 309,
+    InputKey_F21 = 310,
+    InputKey_F22 = 311,
+    InputKey_F23 = 312,
+    InputKey_F24 = 313,
+    InputKey_F25 = 314,
+    InputKey_KP0 = 320,
+    InputKey_KP1 = 321,
+    InputKey_KP2 = 322,
+    InputKey_KP3 = 323,
+    InputKey_KP4 = 324,
+    InputKey_KP5 = 325,
+    InputKey_KP6 = 326,
+    InputKey_KP7 = 327,
+    InputKey_KP8 = 328,
+    InputKey_KP9 = 329,
+    InputKey_KP_Decimal = 330,
+    InputKey_KP_Divide = 331,
+    InputKey_KP_Multiply = 332,
+    InputKey_KP_Subtract = 333,
+    InputKey_KP_Add = 334,
+    InputKey_KP_Enter = 335,
+    InputKey_KP_Equal = 336,
+    InputKey_LeftShift = 340,
+    InputKey_LeftControl = 341,
+    InputKey_LeftAlt = 342,
+    InputKey_LeftSuper = 343,
+    InputKey_RightShift = 344,
+    InputKey_RightControl = 345,
+    InputKey_RightAlt = 346,
+    InputKey_RightSuper = 347,
+    InputKey_Menu = 348,
+    InputKey_Count,
+} InputKeyE;
 
 typedef void (*BootstrapInitPtr)();
 typedef void (*BootstrapUpdatePtr)(f32 dt);
@@ -74,14 +199,18 @@ typedef struct BootstrapEngineSettingsT
 // Creates an opengl context
 void bootstrap_init(BootstrapWindowSettingsT window_settings,
                     BootstrapEngineSettingsT engine_settings);
-
 // Start the main render & update loop
 void bootstrap_run();
-
 // Stop the main loop and close the window
 void bootstrap_stop();
-
 // Current time in seconds
 f32 bootstrap_time();
+// Input event helpers
+bool bootstrap_key_pressed(InputKeyE key);
+bool bootstrap_key_released(InputKeyE key);
+bool bootstrap_key_down(InputKeyE key);
+bool bootstrap_mouse_pressed(MouseButtonE button);
+bool bootstrap_mouse_released(MouseButtonE button);
+bool bootstrap_mouse_down(MouseButtonE button);
 
 #endif
